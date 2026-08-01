9dc31816e6c2d4b506ccd2529bbe8534acceb616
DavidAjayiA <davidajayi006@email.com>
2026-08-01 02:39:47 +0100

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/React-TicketApp-main/',
+  base: '/React-TicketApp/',
   plugins: [react()],
   server: { open: true }
 })
